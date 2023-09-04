f4fa72ee62e86fb5894fcab46529bdbc8fce0c04
Shahrear-ahamed <its.shahrear@gmail.com>
2023-09-04 16:37:57 +0600

    [DONE] profile section are added

---
+++ src/app/modules/profile/profile.controller.ts
@@ -0,0 +1,23 @@
+import httpStatus from "http-status";
+import { JwtPayload } from "jsonwebtoken";
+import catchAsync from "../../../shared/catchAsync";
+import sendResponse from "../../../shared/sendResponse";
+import { ProfileService } from "./profile.service";
+// Your controller code here
+
+const getProfile = catchAsync(async (req, res) => {
+  const { user } = req;
+
+  const result = await ProfileService.getProfile(user as JwtPayload);
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Profile fetched successfully",
+    data: result,
+  });
+});
+
+export const ProfileController = {
+  getProfile,
+};
---
+++ src/app/modules/profile/profile.service.ts
@@ -0,0 +1,27 @@
+// Your service code here
+
+import { JwtPayload } from "jsonwebtoken";
+import prisma from "../../../shared/prisma";
+
+const getProfile = async (user: JwtPayload) => {
+  return await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      role: true,
+      contactNo: true,
+      address: true,
+      profileImg: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+};
+
+export const ProfileService = {
+  getProfile,
+};
